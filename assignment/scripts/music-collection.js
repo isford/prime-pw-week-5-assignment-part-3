a03f92b04a0f7e8039d612bad1fedf3a41ac4070
Ian <isford7@gmail.com>
2021-05-02 14:36:58 -0500

    Done with pt3 and a stretch goal

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,8 +2,8 @@ console.log('***** Music Collection *****')
 
 let collection = []
 function addToCollection( title, artist, yearPublished) {
-  collection.push({title, artist, yearPublished});
-  return {title, artist, yearPublished}
+  collection.push({title, artist, yearPublished});//adds music to collection array
+  return {title, artist, yearPublished}//returns input
 }
 
 console.log( addToCollection('Money', 'Pink Floyd',
@@ -26,8 +26,41 @@ console.log( addToCollection('Down to the River', 'Brown Bird',
 
 console.log('The total collection array', collection);
 
-function showCollection(){
-return (title + 'by' + artist + 'published in' + yearPublished);
+
+function showCollection(array){
+  for (i = 0; i < array.length; i++){
+    console.log(`${array[i].title} by ${array[i].artist}, published in ${array[i].yearPublished}.`);
+  }
+}
+showCollection(collection)
+
+function findByArtist(artist){
+  let artistArray = [];
+  for (let i = 0; i < collection.length; i++){
+    if(artist === collection[i].artist){
+      artistArray.push(collection[i]);
+    }
+    return artistArray;
+  }
 }
 
-console.log(showCollection());
+console.log(findByArtist('Pink Floyd'));
+console.log(findByArtist('JavaScript'));
+
+
+function search(searchTerm){
+  let searchTermArray = [];
+  for (let i=0; i < collection.length; i++)
+  if (collection[i].title === searchTerm || collection[i].artist === searchTerm || collection[i].yearPublished === searchTerm){
+    searchTermArray.push(collection[i]);
+    console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}.`);
+    }
+    else {
+      console.log(searchTermArray);
+    }
+    return searchTermArray;
+}
+search('Pink Floyd')
+search('Brown Bird')
+search(2001)
+search('The Beatles')
